6eb47f89a0f4942874f25f4dcfb10630cdc2ce53
Sree <sreevanthduggirala@gmail.com>
2023-11-16 20:13:44 -0600

    reimbursement, member in group check

--- src/Splitwise.sol
+++ src/Splitwise.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.19;
 
 contract SplitwiseStorage {
-    // @notice: Events for expenses
+    // @notice: Expenses
     struct Expense {
         uint256 expenseId;
         string expenseName;
@@ -25,25 +25,25 @@ contract SplitwiseStorage {
 
     event groupFormed(uint256 groupId, address[] members);
     event expenseMade(uint256 groupId, uint256 cost, address[] debtors);
+    event joinedGroup(uint256 groupId, address member);
     event reimbursed(address debtor, address creditor);
 }
 
 contract Splitwise is SplitwiseStorage {
     // @notice: Checks if given address is within the group of the given ID
     // @params: Group ID, an arbitrary wallet address
-    modifier inGroup(uint256 _groupId, address payable _member) {
+    function inGroup(
+        uint256 _groupId,
+        address payable _member
+    ) public view returns (bool) {
         bool isIn;
         for (uint256 i = 0; i < groups[_groupId].members.length; i++) {
             if (groups[_groupId].members[i] == _member) {
                 isIn = true;
-                break;
             }
         }
 
-        if (isIn == false) {
-            revert("Member is not in group");
-        }
-        _;
+        return isIn;
     }
 
     // @notice: Creates new group for tracking IOUs
@@ -55,12 +55,8 @@ contract Splitwise is SplitwiseStorage {
 
     // @notice: Allows users to join pre-existing groups
     function invite(uint256 _groupId, address payable _invitee) public {
-        bool isIn;
-        for (uint256 i = 0; i < groups[_groupId].members.length; i++) {
-            if (groups[_groupId].members[i] == _invitee) {
-                isIn = true;
-                revert("Member is already in group");
-            }
+        if (inGroup(_groupId, _invitee) == true) {
+            revert("Member already in group");
         }
 
         groups[_groupId].members.push(_invitee);
@@ -78,6 +74,23 @@ contract Splitwise is SplitwiseStorage {
     // @params: Group ID, member's wallet address or ENS
     function reimburse(
         uint256 _groupId,
+        uint256 _expenseId,
         address payable _creditor
-    ) public inGroup(_groupId, _creditor) {}
+    ) public payable {
+        if (inGroup(_groupId, _creditor) == false) {
+            revert("User not in this group");
+        } else if (
+            msg.value <
+            groups[_groupId].expenses[_expenseId].amountOwed[msg.sender]
+        ) {
+            revert("Insufficient amount");
+        }
+
+        payable(groups[_groupId].expenses[_expenseId].creditor).transfer(
+            (groups[_groupId].expenses[_expenseId].amountOwed[msg.sender] *
+                98) / 100
+        );
+
+        groups[_groupId].expenses[_expenseId].amountOwed[msg.sender] = 0;
+    }
 }
